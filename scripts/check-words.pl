33c82793928a7b7a4bb9af1d9f5242127f76c32f
Tung Teck Lee <tecklee@gmail.com>
2010-08-28 22:18:07 +0800

    - introduced a skip sheet in source spreadsheet to filter out words that we don't want as new words - updated logic a bit to use canonical_word function more uniformly, in order to centralize the removal of punctations etc - added new punctuation marks like exclamiation to help filter out 'noitce'

--- scripts/check-words.pl
+++ scripts/check-words.pl
@@ -18,13 +18,17 @@ use Data::Dumper;
 # data file name, we assumpt ods files
 my $DATA_FILENAME = "Wordlist20100623.ods";
 
-# The target sheet in the spreadsheet where you store the 
-# words 
-my $TARGET_SHEET = "Word List";
-
-# The columns in spreadsheet to read, note that first column is 
-# column 1 not column zero
-my $TARGET_COL = 3;
+# structure to store sheets and columns to read for words in database
+my $SHEETS = [
+  {
+    'sheet' => "Word List",
+    'col' => 3,
+  },
+  {
+    'sheet' => "skip",
+    'col' => 1,    
+  },
+];
 
 # this should be a plain text file used to check for new 
 # words. it would be easier if you just cut-and-paste
@@ -38,6 +42,7 @@ print "Reading DB\n";
 
 # read data in, surpress populating of individual named cells
 my $ref = ReadData($DATA_FILENAME, cells => false);
+
 
 # sample read format
 # $ref = [
@@ -67,43 +72,48 @@ my $ref = ReadData($DATA_FILENAME, cells => false);
 #           :
 #         :
 
-print "Number of words read: ", read_word_count($ref), "\n";
+foreach $sheet (@$SHEETS) {
+  print "Number of words read: ", read_word_count($ref,$sheet), "\n";
+  
+  for ($i=1; $i < read_word_count($ref,$sheet); $i++) {
+    $word = read_word_idx($ref,$sheet,$i);
+    
+    $word_idx=canonical_word($word);
+    #print "word=$word\n";
+    
+    # store orginal content
+    $content{$word_idx} = $word;      
+    #last if $count++ > 5;
+    
+    # rudimentary parsing
+    # we need to account for the input 'word' being
+    # actually a phrase or sentence, in which case
+    # we store the component words 
 
-for ($i=1; $i < read_word_count($ref); $i++) {
-  $word = read_word_idx($ref, $i);
-  
-  $word_idx=lc($word);
-  #print "word=$word\n";
-  
-  # store orginal content
-  $content{$word_idx} = $word;      
-  #last if $count++ > 5;
-  
-  # rudimentary parsing
-  # we need to account for the input 'word' being
-  # actually a phrase or sentence, in which case
-  # we store the component words 
-
-  @words_arr = split(/\s+/, $word);
-  foreach $w (@words_arr) {
-    # get rid of common punctuactions
-    $w =~ s/[.,\"\-\?\:]//g;
-    next if $word eq "";
-    $words_idx{$w} = $word;
-    #print "sav $w : $word\n";
-  } 
+    @words_arr = split(/\s+/, $word);
+    foreach $w (@words_arr) {
+      # get rid of common punctuactions
+      $w = canonical_word($w);
+      next if $w eq "";
       
-  #print "[$i]\t", $word, "\n";
-  #last if ($i ==10);
+      #print "XXX: in $w\n" if ($word =~ /special/);
+      $content{$w} = $word;
+      #$words_idx{$w} = $word;
+      #print "sav $w : $word\n";
+    } 
+        
+    #print "[$i]\t", $word, "\n";
+    #last if ($i ==10);
+  }
+  print "DB total: $i lines...\n";  
 }  
-
-print "DB total: $i lines...\n";
 
 my $fh = new IO::File($INPUT_FILENAME);
 defined($fh) or die "cannot open file $INPUT_FILENAME: $!";
 
 my $i = 0;
 my $iNotFound=0;
+my $LIKE = "";
 while (<$fh>) {
   chomp $_;     
   @words_arr = split(/\s+/, $_);
@@ -113,14 +123,17 @@ while (<$fh>) {
         #print "$w => $found\n";
     } else {
       if ($found !~ /^FOUND/) {
-        if (!$not_found{$w}) {
+        if (!$not_found{canonical_word($w)}) {
           # we don't want to repeat the printout for same word
-          print ++$iNotFound, " NOT FOUND $w\n";          
-          $not_found{$w} = $w;
+          #print ++$iNotFound, " NOT FOUND $w\n";
+          ++$iNotFound;
+          $FOUND_WORDS .= "$w\n";          
+          $not_found{canonical_word($w)} = $w;
         }        
       } else {
         if ($found =~ /like/) {
-          print "  LIKE [$found]\n";
+          #print "  LIKE [$found]\n";
+          $LIKE .= "  - [$found]\n";
         }
         #print "FOUND [$w]\n";
       }
@@ -129,6 +142,11 @@ while (<$fh>) {
   $i++;
   #last if $i++ > 5;
 }
+
+print "=== list of words not found (count=$iNotFound) ===\n";
+print $FOUND_WORDS,"\n";
+print "=== list of similar ===\n";
+print $LIKE,"\n";
 print "END checked $i lines\n";
 
 
@@ -138,17 +156,25 @@ print "END checked $i lines\n";
 #  print "sentence=[$sentence] FOUND!";
 #} 
 
+sub canonical_word {
+  my $w = $_[0];
+  $w =~ s/[`“”\.\,\!\"\-\?\:\(\)\']//g;
+  
+  return lc($w);
+}
+
+
 sub check_word {
   my $word = $_[0];
-  my $w = $word;
-  $w =~ s/[.,\"\-\?\:\(\)]//g;
-  if ($w eq "") {
+  my $w = canonical_word($word);
+  #$w =~ s/[.,\"\-\?\:\(\)]//g;
+  if ( ($w eq "") || ($w =~ /^[0-9\,]*$/)) {
    return "FOUND";
   }
     
   my $ret;
   #print "check word=$word\n";
-  if ($content{lc($w)}) {
+  if ($content{$w}) {
     #print "check word=$word", "=>found\n";
     $ret="FOUND";
     return $ret;    
@@ -165,7 +191,8 @@ sub check_word {
   [qr/ed$/i, "e"],  # words like released
   [qr/s$/i, ""],   # words like plays  
   [qr/ies$/i, "y"],    # words like flies
-  [qr/ing$/i, ""],    # words like playing  
+  [qr/ing$/i, ""],    # words like playing
+  [qr/ly$/i, ""],    # words like bitterly  
   );
 
   foreach $r (@subs) {
@@ -173,32 +200,35 @@ sub check_word {
     $w2 = $w;
     $w2 =~ s/$r->[0]/$r->[1]/i;
     # check against the variant
-    if ($content{lc($w2)}) {
+    if ($content{$w2}) {
       $ret = "FOUND $word like $w2";
       #print "XXX", $ret, "\n";
       return $ret;
     }    
   }
   
-  my $c = $words_idx{$w};
-  if ($c && $c ne $w) {
-    #print "$word related=$c\n";
-    $ret = "RELATED to [$c]";
-  }
-  return $ret;
+  #my $c = $words_idx{$w};
+  #if ($c && $c ne $w) {
+  #  #print "$word related=$c\n";
+  #  $ret = "RELATED to [$c]";
+  #}
+  #return $ret;
 }
 
 # define some helper apps to abstract the references to the sheets  
 sub read_word_count() {
   my $ref = shift;
-  my $sheet_num = $ref->[0]{sheet}{$TARGET_SHEET};
+  my $sheet = shift;
+  
+  my $sheet_num = $ref->[0]{sheet}{$sheet->{'sheet'}};
   return $ref->[$sheet_num]{maxrow}; 
 }         
 
 sub read_word_idx() {
-  my($ref,$idx) = @_;
-  my $sheet_num = $ref->[0]{sheet}{$TARGET_SHEET};
+  my($ref,$sheet,$idx) = @_;
+  my $sheet_num = $ref->[0]{sheet}{$sheet->{'sheet'}};
   #$cell = cr2cell(2,$ix);
   #print "getting cell $cell\n";
-  return $ref->[$sheet_num]{cell}[$TARGET_COL][$idx];
+  return $ref->[$sheet_num]{cell}[$sheet->{'col'}][$idx];
 }
+
